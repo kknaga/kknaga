3e171a01cf9bac11259656b78f7d7e1bc0d40539
Kareem <kareemkamal91@gmail.com>
2025-04-02 10:49:49 +0200

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Serif, Noto_Sans } from "next/font/google";
 import "./globals.scss";
 import Navigation from "./Navigation";
+import Script from "next/script";
 
 const sans = Noto_Sans({
   subsets: ["latin"],
@@ -14,6 +15,8 @@ const serif = Noto_Serif({
   variable: "--font-serif",
   display: "swap",
 });
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const metadata: Metadata = {
   title: "Kareem Aboualnaga - Software Engineer",
@@ -30,6 +33,30 @@ export default function RootLayout({
       <body className={`${sans.variable} ${serif.variable}`}>
         <Navigation />
         {children}
+
+        {GA_TRACKING_ID && process.env.NODE_ENV === "production" && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
       </body>
     </html>
   );
